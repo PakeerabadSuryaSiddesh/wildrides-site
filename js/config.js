1cf581f294ba3c0401612d1c47e5691c568e1508
Pakeerabad Surya Siddesh <suryapk2002@gmail.com>
2025-04-10 16:17:39 +0530

    secret key updated

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-south-1_CqKKnsRt9', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7qq6cd0put6g8v7p6sksl6n2ep', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '1b9vo0au1dq2o9dsun7epjh91s8apfc1674r5jq9rmo3478b0pf6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
