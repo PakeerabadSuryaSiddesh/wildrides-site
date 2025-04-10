8b06adbf1f1ebdf9688a8336b62df079e30a06ab
Pakeerabad Surya Siddesh <suryapk2002@gmail.com>
2025-04-10 15:59:10 +0530

    cognito pool and clinet ID updated in config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-south-1_CqKKnsRt9', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7qq6cd0put6g8v7p6sksl6n2ep', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
